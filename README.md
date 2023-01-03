492d6577810bc5e6a650d788ab7420b96475f612
leothorp <leothorp@users.noreply.github.com>
2023-01-02 22:50:35 -0600

    script

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # podcast-transcriber
+CLI tool to transcribe podcast audio files (or other arbitrary English audio with multiple speakers.) Uses the Deepgram API.
+
+## Usage
+
+### Initial Setup / Prerequisites
+* Install [Node.js](https://nodejs.org/en/download/) and [git](https://git-scm.com/download).
+* Obtain audio files for the podcast (or other speech audio) that you want to transcribe. Put them in a directory on your local machine. If the original source is Spotify, you can use a tool like [TuneFab](https://www.tunefab.com/) to get these files.
+* Clone this repository to your local machine:
+```
+git clone https://github.com/leothorp/podcast-transcriber.git
+```
+* Sign up for a [Deepgram account](https://console.deepgram.com/signup) and obtain an API key. As of 12/29/22, this includes $150 worth of free API credits.
+* Create a file called `.env` at the root of this repo (this file will be gitignored.)
+Add your Deepgram API key to this file as follows:
+```
+DEEPGRAM_API_KEY=insert-api-key-value-here
+```
+* run `npm install` inside this project directory.
+
+### Generating Transcriptions
+1. After completing the prerequisites above, run the tool on your directory of audio files as follows:
+```
+npm start path/to/audio/directory
+```
+
+## Limitations
+* Currently this script can only be used on wav, mp3, ogg, or m4a files. mp3 is recommended.
+
+
+
---
+++ index.js
@@ -0,0 +1,55 @@
+const { Deepgram } = require("@deepgram/sdk");
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime");
+
+console.log(process.argv);
+const fileFormats = [".wav", ".mp3", ".ogg", ".m4a"];
+
+const audioFileDir = process.argv[2];
+const timestampStr = new Date().toISOString().replace(/:/g, "");
+const transcriptionsDir = path.join(
+  path.dirname(audioFileDir),
+  path.basename(audioFileDir) + `_transcriptions_${timestampStr}`
+);
+console.log(transcriptionsDir);
+const transcriptionJsonDir = path.join(transcriptionsDir, "json");
+const transcriptionPdfsDir = path.join(transcriptionsDir, "pdfs");
+fs.mkdirSync(transcriptionsDir);
+fs.mkdirSync(transcriptionJsonDir);
+fs.mkdirSync(transcriptionPdfsDir);
+// @ts-ignore
+const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
+
+fs.readdirSync(audioFileDir).forEach(async (fileName) => {
+  const filePath = path.join(audioFileDir, fileName);
+  if (!fileFormats.includes(path.extname(filePath))) {
+    //ignore all other files
+    return;
+  }
+  console.log("path", filePath);
+  const buffer = fs.readFileSync(filePath);
+  const audioSource = {
+    buffer,
+    // best guess for mimetype if not derivable
+    mimetype: mime.getType(filePath) || "audio/mpeg",
+  };
+  console.log(audioSource);
+  const response = await deepgram.transcription.preRecorded(audioSource, {
+    punctuate: true,
+    paragraphs: true,
+    dates: true,
+    times: true,
+    numbers: true,
+    measurements: true,
+    smart_format: true,
+    diarize: true,
+    tier: "enhanced",
+    utterances: true,
+  });
+  const jsonFileName = fileName.split(".").slice(0, -1).join(".") + ".json";
+  fs.writeFileSync(
+    path.join(transcriptionJsonDir, jsonFileName),
+    JSON.stringify(response)
+  );
+});
